3937c1c97f813c08c6857025de737214dbd28798
Rustingheart <hierzi.p@gmail.com>
2024-10-20 14:48:43 +0200

    bf server_ip

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "DST-Server-IP" {
+output "Server_IP" {
   value = var.server_ip_tf
-  description = "The IP address of the DST server."
+  description = "The IP address of the DST or other server."
 }
